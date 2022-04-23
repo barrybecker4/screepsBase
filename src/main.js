d721e5d499057f59ae564ef141a92d37f07a72c3
Barry Becker <barrybecker4@gmail.com>
2022-04-23 08:15:46 -0700

    maintain creeps

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
-var roleHarvester = require('role.harvester');
-var roleUpgrader = require('role.upgrader');
-var roleBuilder = require('role.builder');
+var roleHarvester = require('roles_role.harvester');
+var roleUpgrader = require('roles_role.upgrader');
+var roleBuilder = require('roles_role.builder');
 
 /**
  * One off scripts for console
- * Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], 'harvester1', { memory:{ role: 'harvester' }} );
+ * Game.spawns['Spawn1'].spawnCreep( [WORK, WORK, CARRY, MOVE], 'harvester1', { memory:{ role: 'harvester' }} );
  * Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], 'harvester2', { memory:{ role: 'harvester' }} );
- * Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], 'harvester2', { memory:{ role: 'builder' }} );
+ * Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], 'builder1', { memory:{ role: 'builder' }} );
+ * Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], 'upgrader1', { memory:{ role: 'upgrader' }} );
  *
  module.exports.loop = function () {
      var creep = Game.creeps['Harvester1'];
@@ -21,6 +22,25 @@ module.exports.loop = function () {
     // This will send emails to you when executed.
     // Game.notify(`Does this show? User bb4`);
 
+    if (!Game.creeps["harvester1"]) {
+        Game.spawns['Spawn1'].spawnCreep( [WORK, WORK, CARRY, MOVE], 'harvester1', { memory:{ role: 'harvester' }} );
+    }
+
+    for (var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        console.log("creep " + JSON.stringify(creep.memory));
+        if (creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
+        }
+        if (creep.memory.role == 'upgrader') {
+            roleUpgrader.run(creep);
+        }
+        if (creep.memory.role == 'builder') {
+            roleBuilder.run(creep);
+        }
+    }
+
+    // Once the base is established we can create and maintain a tower.
     var tower = Game.getObjectById('7ae49ba57f35359f19916d70');
     if (tower) {
         const closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
@@ -36,17 +56,4 @@ module.exports.loop = function () {
         }
     }
 
-    for (var name in Game.creeps) {
-        var creep = Game.creeps[name];
-        console.log("creep " + JSON.stringify(creep.memory));
-        if (creep.memory.role == 'harvester') {
-            roleHarvester.run(creep);
-        }
-        if (creep.memory.role == 'upgrader') {
-            roleUpgrader.run(creep);
-        }
-        if(creep.memory.role == 'builder') {
-            roleBuilder.run(creep);
-        }
-    }
 }
--- src/roles/role.harvester.js
+++ src/roles/role.harvester.js
@@ -2,23 +2,27 @@ var roleHarvester = {
 
     run: function(creep) {
         if (creep.store.getFreeCapacity() > 0) {
-              const sources = creep.room.find(FIND_SOURCES);
-              if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                  creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
-              }
+            console.log("harvester freeCap: " + creep.store.getFreeCapacity());
+            const sources = creep.room.find(FIND_SOURCES);
+            if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                console.log("Not in range! moving there");
+                creep.moveTo(sources[0], { visualizePathStyle: {stroke: '#ffaa00'} });
+            }
         }
         else {
             const targets = creep.room.find(FIND_STRUCTURES, {
                 filter: (structure) => {
+                    console.log("structure: " + JSON.stringify(structure));
                     return (structure.structureType == STRUCTURE_EXTENSION ||
                         structure.structureType == STRUCTURE_SPAWN ||
                         structure.structureType == STRUCTURE_TOWER) &&
                         structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
                 }
             });
+            console.log("targets: " + targets.length);
             if (targets.length > 0) {
                 if (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                    creep.moveTo(targets[0], { visualizePathStyle: {stroke: '#ffffff'} });
                 }
             }
         }
